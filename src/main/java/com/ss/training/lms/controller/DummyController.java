0b96d61a13ca7f8222214e7594acd4cebd503f25
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-12 12:02:44 -0600

    Set up dummy controller class with dummy endpoints

---
+++ src/main/java/com/ss/training/lms/controller/DummyController.java
@@ -0,0 +1,27 @@
+package com.ss.training.lms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Justin O'Brien
+ */
+@RestController
+public class DummyController {
+
+	@RequestMapping(path = "/lms/librarian/dummy")
+	public String librarianDummy() {
+		return "Librarian page";
+	}
+
+	@RequestMapping(path = "/lms/borrower/dummy")
+	public String borrowerDummy() {
+		return "Borrower page";
+	}
+
+	@RequestMapping(path = "/lms/admin/dummy")
+	public String adminDummy() {
+		return "Admin page";
+	}
+
+}
