c9da21b564d16f80a8321dfe8e13ae0df7ebb21f
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-05-14 16:01:12 -0500

    add @crossOrigin

--- src/main/java/com/ss/training/lms/orchestrator/BorrowerOrchestrator.java
+++ src/main/java/com/ss/training/lms/orchestrator/BorrowerOrchestrator.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@CrossOrigin
 public class BorrowerOrchestrator {
 
     @Autowired
